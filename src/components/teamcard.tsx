b36daf056026fc84f672b5fd8122cf2679577fca
jcklerk <justin@klerk.nl>
2025-09-06 02:09:56 +0200

    uit

--- src/components/teamcard.tsx
+++ src/components/teamcard.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/mhcLogoWhite.svg";
+// import logo from "@/assets/mhcLogoWhite.svg";
 
 export interface Team {
 	name: string;
@@ -9,7 +9,14 @@ export interface Team {
 export default function TeamCard({ team }: { team: Team }) {
 	return (
 		<div className="flex flex-col items-center">
-			<img src={team.logo || logo} alt="" className="w-34 h-34 mb-2" />
+			<img
+				src={
+					team.logo ||
+					"https://storage.googleapis.com/publiq_be_production/files/projecten/UDB/_660x660_crop_center-center_82_line/UiTlabel.png"
+				}
+				alt=""
+				className="w-34 h-34 mb-2"
+			/>
 			<span className="font-bold text-xl">{team.name}</span>
 		</div>
 	);
