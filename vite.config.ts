b4f82af2796b5777093315aabc67319da1e77881
jcklerk <justin@klerk.nl>
2025-10-17 19:21:15 +0200

    fix ?

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
-import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: true,
-    allowedHosts: true,
-  },
-})
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		host: "0.0.0.0",
+		allowedHosts: ["."], // equivalent to "all hosts" for some older Vite versions
+	},
+});
